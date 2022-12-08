8c03a14b56e44121bc14b46fbe2dce314edb0ddd
Vojtěch Struhár <vojtechstruhar@gmail.com>
2022-12-08 22:52:08 +0100

    SyntaxHighlighter build fails

--- src/components/CodeSnippet.tsx
+++ src/components/CodeSnippet.tsx
@@ -1,3 +1,15 @@
+
+import SyntaxHighlighter from 'react-syntax-highlighter';
+// esm errors, but cjs works
+// https://github.com/react-syntax-highlighter/react-syntax-highlighter/issues/230#issuecomment-568377353
+import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+
+const codeString = '(num) => num + 1';
+
 export default function CodeSnippet() {
-    return <div>code snippet</div>
+    return <div>code snippet
+        <SyntaxHighlighter language="javascript" style={a11yDark}>
+            {codeString}
+        </SyntaxHighlighter>
+    </div>
 }
